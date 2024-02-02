0c6944ee7c0d9bc4679f7eb53a47d22714aa2abd
Danny Oka <dannyoka@gmail.com>
2024-02-02 00:11:44 -0700

    added git plugins

--- lua/plugins/git-plugins.lua
+++ lua/plugins/git-plugins.lua
@@ -11,6 +11,7 @@ return {
 			require("gitsigns").setup({})
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.keymap.set("n", "<leader>gcc", ":G<CR>", {})
 		end,
 	},
 }
