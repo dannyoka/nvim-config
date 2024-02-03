bf5f75a574bdf3ee7530207e4b8341ee4d4516cf
Danny Oka <dannyoka@gmail.com>
2024-02-03 12:30:18 -0700

    adds lazygit keymap

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -3,5 +3,8 @@ return {
   -- optional for floating window border decoration
   requires = {
     "nvim-lua/plenary.nvim",
-  }
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>")
+  end 
 }
