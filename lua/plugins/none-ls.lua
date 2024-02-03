1e6a4905bd37b93d176024644a87d97472c08b17
Danny Oka <dannyoka@gmail.com>
2024-02-03 14:34:49 -0700

    adds in go formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,8 +9,20 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.golines,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      desc = 'Format before save',
+      pattern = '*',
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
   end,
 }
