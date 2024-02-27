6fa748149df8dd1829d28214fe4076768ce57dbb
Danny Oka <dannyoka@gmail.com>
2024-02-27 10:26:07 -0700

    auto inline blames

--- lua/plugins/git-plugins.lua
+++ lua/plugins/git-plugins.lua
@@ -5,7 +5,15 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup({})
+      local current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol",
+        delay = 200,
+      }
+      require("gitsigns").setup({
+        current_line_blame = true,
+        current_line_blame_opts = current_line_blame_opts,
+      })
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
       vim.keymap.set("n", "<leader>gc", ":G<CR>", {})
