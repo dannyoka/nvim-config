c320bd55d0dec5f6f596d820b61b0132796797e1
Danny Oka <dannyoka@gmail.com>
2024-04-13 23:05:29 -0600

    updates oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,16 +1,20 @@
 return {
-  'stevearc/oil.nvim',
-  config = function()
-    require("oil").setup({
-      view_options = {
-        show_hidden = true
-      },
-      keymaps = {
-        ["<C-h>"] = false,
-        ["<C-p>"] = false,
-        ["<C-s>"] = false,
-      },
-    })
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>")
-  end
+	"stevearc/oil.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+			keymaps = {
+				["<C-h>"] = false,
+				["<C-p>"] = false,
+				["<C-s>"] = false,
+			},
+			default_file_explorer = true,
+		})
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+	end,
 }
