e3aeed569114e6f49a0c28d1e2bd0ee0a52be169
Danny Oka <dannyoka@gmail.com>
2024-04-13 23:31:50 -0600

    adds debug and fine cmdline

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -3,10 +3,22 @@ return {
   dependencies = {
     "rcarriga/nvim-dap-ui",
     "leoluz/nvim-dap-go",
-    "nvim-neotest/nvim-nio"
+    "nvim-neotest/nvim-nio",
+    "mxsdev/nvim-dap-vscode-js",
+    {
+      "microsoft/vscode-js-debug",
+      opt = true,
+      run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
+    },
   },
   config = function()
     local dap, dapui = require("dap"), require("dapui")
+    require("dapui").setup()
+    require("dap-go").setup()
+    require("dap-vscode-js").setup({
+      adapters = { 'pwa-node', 'pwa-chrome', 'node' },
+    })
+    require('dap.ext.vscode').load_launchjs(nil, { node = { 'javascript', 'typescript' } })
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
@@ -19,8 +31,10 @@ return {
     dap.listeners.before.event_exited.dapui_config = function()
       dapui.close()
     end
-    vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<leader>dt", dapui.toggle, {})
     vim.keymap.set("n", "<leader>dc", dap.continue, {})
+    vim.keymap.set("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", {})
   end
 
 }
