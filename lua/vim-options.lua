1a6bb9ae61e7108925e3a05f6db2148190acdca0
Danny Oka <dannyoka@gmail.com>
2024-04-20 00:02:41 -0600

    test-commit

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -25,15 +25,15 @@ local opts = {
 
 require("lazy").setup("plugins", opts)
 
--- local function augroup(name)
---   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
--- end
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
 
--- vim.api.nvim_create_autocmd("VimEnter", {
---   group = augroup("autoupdate"),
---   callback = function()
---     require("lazy").update({
---       show = false,
---     })
---   end,
--- })
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = augroup("autoupdate"),
+  callback = function()
+    require("lazy").update({
+      show = false,
+    })
+  end,
+})
