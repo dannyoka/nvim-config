ac2e602dadad9eda0fc569e3968285a4f456558e
Danny Oka <dannyoka@gmail.com>
2024-05-31 23:22:39 -0600

    updates

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -47,6 +47,9 @@ return {
 			lspconfig.intelephense.setup({
 				capabilities = capabilities,
 			})
+			lspconfig.graphql.setup({
+				capabilities = capabilities,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
