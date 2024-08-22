7a569c38313bff2045d189c6dffa6762146c226a
Danny Oka <dannyoka@gmail.com>
2024-08-21 19:14:02 -0600

    adds octo stuff

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -7,6 +7,9 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("octo").setup()
+    require("octo").setup({
+      enable_builtin = true,
+    })
+    vim.keymap.set("n", "<leader>o", "<cmd>Octo<cr>", { noremap = true, silent = true })
   end,
 }
--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,12 +1,11 @@
 return {
-  'vim-test/vim-test',
-  dependencies = {
-    'preservim/vimux'
-  },
-  vim.keymap.set('n', '<leader>t', ':TestNearest<CR>'),
-  vim.keymap.set('n', '<leader>T', ':TestFile<CR>'),
-  vim.keymap.set('n', '<leader>ts', ':TestSuite<CR>'),
-  vim.keymap.set('n', '<leader>l', ':TestLast<CR>'),
-  vim.keymap.set('n', '<leader>g', ':TestVisit<CR>'),
-  vim.cmd('let test#strategy = "vimux"')
+	"vim-test/vim-test",
+	dependencies = {
+		"preservim/vimux",
+	},
+	vim.keymap.set("n", "<leader>t", ":TestNearest<CR>"),
+	vim.keymap.set("n", "<leader>T", ":TestFile<CR>"),
+	vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>"),
+	vim.keymap.set("n", "<leader>l", ":TestLast<CR>"),
+	vim.cmd('let test#strategy = "vimux"'),
 }
