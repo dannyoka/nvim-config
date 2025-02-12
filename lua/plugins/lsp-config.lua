cbc812b5dbfc01f209cd54a4dd8689a9bebf27fd
Danny Oka <dannyoka@gmail.com>
2025-02-11 23:48:24 -0700

    new stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,61 +1,64 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		auto_install = true,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-					"gopls",
-					"pyright",
-					"intelephense",
-					"graphql",
-					-- "emmet_ls",
-					"emmet_language_server",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.graphql.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.intelephense.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.graphql.setup({
-				capabilities = capabilities,
-			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>err", vim.diagnostic.open_float, {})
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    auto_install = true,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "gopls",
+          "pyright",
+          "intelephense",
+          "graphql",
+          -- "emmet_ls",
+          "emmet_language_server",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.graphql.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.intelephense.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.graphql.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.emmet_language_server.setup({
+        capabilities = capabilities,
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>err", vim.diagnostic.open_float, {})
+    end,
+  },
 }
--- lua/plugins/nvim-clip.lua
+++ lua/plugins/nvim-clip.lua
@@ -3,10 +3,17 @@ return {
 	dependencies = {
 		{ "kkharji/sqlite.lua", module = "sqlite" },
 		-- you'll need at least one of these
-		-- {'nvim-telescope/telescope.nvim'},
+		{ "nvim-telescope/telescope.nvim" },
 		-- {'ibhagwan/fzf-lua'},
 	},
 	config = function()
-		require("neoclip").setup()
+		require("neoclip").setup({
+			default_register = "*",
+			on_paste = {
+				close_telescope = false,
+			},
+			continuous_sync = true,
+		})
+		vim.keymap.set("n", "<leader>pp", "<cmd>Telescope neoclip<CR>")
 	end,
 }
