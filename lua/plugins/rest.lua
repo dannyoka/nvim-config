e3698adaa8335d12a1f7c5591a4833d041f2e2db
Danny Oka <dannyoka@gmail.com>
2025-03-30 17:00:49 -0600

    remove plugins

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,21 +1,21 @@
 return {
-	{
-		"vhyrro/luarocks.nvim",
-		priority = 1000,
-		config = true,
-		opts = {
-			rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-		},
-	},
-	{
-		"rest-nvim/rest.nvim",
-		ft = "http",
-		dependencies = { "luarocks.nvim" },
-		config = function()
-			require("rest-nvim").setup()
-			-- vim.keymap.set("n", "rr", "<cmd>Rest run<CR>")
-			-- vim.keymap.set("n", "renv", "<cmd>Telescope rest select_env<CR>")
-			require("telescope").load_extension("rest")
-		end,
-	},
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+    },
+  },
+  {
+    "rest-nvim/rest.nvim",
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require("rest-nvim").setup()
+      -- vim.keymap.set("n", "rr", "<cmd>Rest run<CR>")
+      -- vim.keymap.set("n", "renv", "<cmd>Telescope rest select_env<CR>")
+      require("telescope").load_extension("rest")
+    end,
+  },
 }
